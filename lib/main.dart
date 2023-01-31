0717858ca55fc306c209c05a92b560216ed4bc94
Gabriel Jose <gabrieljose528@gmail.com>
2023-01-30 22:45:49 -0300

    Add sound play functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,27 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: MaterialButton(
+            onPressed: () async {
+              final player = AudioPlayer();
+              await player.setSource(AssetSource('note1.wav'));
+              player.resume();
+            },
+            enableFeedback: false,
+            child: const Text('Click me!'),
+          ),
+        ),
+      ),
+    );
+  }
+}
