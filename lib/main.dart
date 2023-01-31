face4cc02464fb881388e7fbda190593c9daaf1e
Gabriel Jose <gabrieljose528@gmail.com>
2023-01-31 20:37:37 -0300

    Changed app name

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:xilofone_flutter/xylobutton.dart';
 
@@ -11,20 +10,27 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+          backgroundColor: Colors.black,
           body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: const [
-            XyloButton(xyloButtonColor: Colors.red, soundPath: 'note1.wav'),
-            XyloButton(xyloButtonColor: Colors.orange, soundPath: 'note2.wav'),
-            XyloButton(xyloButtonColor: Colors.yellow, soundPath: 'note3.wav'),
-            XyloButton(xyloButtonColor: Colors.green, soundPath: 'note4.wav'),
-            XyloButton(xyloButtonColor: Colors.teal, soundPath: 'note5.wav'),
-            XyloButton(xyloButtonColor: Colors.blue, soundPath: 'note6.wav'),
-            XyloButton(xyloButtonColor: Colors.purple, soundPath: 'note7.wav'),
-          ],
-        ),
-      )),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: const [
+                XyloButton(xyloButtonColor: Colors.red, soundPath: 'note1.wav'),
+                XyloButton(
+                    xyloButtonColor: Colors.orange, soundPath: 'note2.wav'),
+                XyloButton(
+                    xyloButtonColor: Colors.yellow, soundPath: 'note3.wav'),
+                XyloButton(
+                    xyloButtonColor: Colors.green, soundPath: 'note4.wav'),
+                XyloButton(
+                    xyloButtonColor: Colors.teal, soundPath: 'note5.wav'),
+                XyloButton(
+                    xyloButtonColor: Colors.blue, soundPath: 'note6.wav'),
+                XyloButton(
+                    xyloButtonColor: Colors.purple, soundPath: 'note7.wav'),
+              ],
+            ),
+          )),
     );
   }
 }
